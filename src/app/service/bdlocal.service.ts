6cecd1ec50c5e30a9844a32870d4ac3ee9833b41
jord.tapiar <jord.tapiar@duocuc.cl>
2024-01-17 01:04:01 -0300

    cambio

--- src/app/service/bdlocal.service.ts
+++ src/app/service/bdlocal.service.ts
@@ -2,19 +2,29 @@ import { Injectable } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 import { Storage } from '@ionic/storage-angular';
 import { IEdicion } from '../interfaces/i-edicion';
+import { loginI } from '../modelos/login.interface';
+import { ResponseI } from '../modelos/response.interface';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BdlocalService {
 
+  url:string ="https://jsonplaceholder.typicode.com/users"
   edicion: IEdicion[]=[];
   private _storage: Storage | null=null;
 
-  constructor(private storage: Storage,public toastController:ToastController) {
+  constructor(private storage: Storage,public toastController:ToastController, private http:HttpClient) {
     this.Init();
     this.cargarContactos();
 }
+  loginByEmail(form:loginI):Observable<ResponseI>{
+    let direccion = this.url + "users";
+    return this.http.post<ResponseI>(direccion,form);
+  }
+
 async Init(){
   const storage=await this.storage.create();
   this._storage=storage;
@@ -30,7 +40,7 @@ guardarContactos(nombre:string,numero:number){
   const existe=this.edicion.find(m=>m.intNumero===numero);
   if (!existe) {
     this.edicion.unshift({strNombre:nombre, intNumero:numero})//inserto en el top
-    this._storage?.set('agenda',this.edicion);
+    this._storage?.set('edicion',this.edicion);
     this.presentToast("Contacto Editado con EXITO!!");
   }else{
     this.presentToast("Contacto Ya editado anteriormente");
@@ -41,7 +51,7 @@ quitarContactos(numero:number){
   const existe=this.edicion.find(m=>m.intNumero===numero);
   if (existe) {
     this.edicion=this.edicion.filter(c=>c.intNumero!==numero);
-    this._storage?.set('agenda',this.edicion);
+    this._storage?.set('edicion',this.edicion);
     this.presentToast("Se ha eliminado la edicion con EXITO!!");
   }else{
     this.presentToast("Ese N° NO Existe en la edicion");
